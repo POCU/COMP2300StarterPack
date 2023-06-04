06e4df3d1f9d5c6f5b58d4e66200aa7799cc5d76
Chris Yoon <78404624+chrisyoon01@users.noreply.github.com>
2023-06-04 01:37:57 -0700

    added lab6 files

---
+++ Lab6/utils/utils.asm
@@ -0,0 +1,27 @@
+    ; assert
+    .MACRO assert ; (addr,value)
+        .IFCONST POBS
+		    .BYTE $FF
+		    .WORD {1}
+		    .BYTE {2}
+        .ELSE
+            lda {1}
+            cmp {2}
+            bne .success
+.fail:
+            lda $BAAD
+            jmp .fail
+.success:
+        .ENDIF
+    .ENDM
+
+    ; terminate program
+    .MACRO termin
+		.IFCONST POBS
+			.BYTE $EF
+		.ELSE
+end:
+        jmp end
+		.ENDIF
+
+    .ENDM
